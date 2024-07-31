5a3c88080e61578805bf78db281ebbf0574aa0c4
Rafael Santos <rafa.05125@gmail.com>
2024-07-31 16:31:14 -0300

    DTOs

---
+++ BlazingBooks.Shared/Dtos/BookDetailsDto.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazingBooks.Shared.Dtos
+{
+    public record BookDetailsDto(int Id, string TItle, string Image, AuthorDto Author,
+                                int NumPages, string Format,
+                                string Description, GenreDto[] Genres);
+}
